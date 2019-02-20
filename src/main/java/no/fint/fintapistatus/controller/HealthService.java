baafbe9218d2487e20cace86009ad268cf09db8b
Ola <olastaalberg@gmail.com>
2019-02-20 11:41:29 +0100

    Oppdatert min WebClient slik at den gir response. Får Hazelcast-feilmelding, spørt hva som er greia med den i Slack

--- src/main/java/no/fint/fintapistatus/controller/HealthService.java
+++ src/main/java/no/fint/fintapistatus/controller/HealthService.java
@@ -12,21 +12,16 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class HealthService {
 
     public static final String healthCheckURL = "https://play-with-fint.felleskomponent.no/utdanning/timeplan/admin/health";
-
     @Autowired
-    WebClient webClient;
-            /*WebClient
-            .builder()
-            .baseUrl("https://play-with-fint.felleskomponent.no/utdanning/timeplan/admin/health")
-            .defaultHeader("x-org-id=health.fintlabs.no").build();*/
+    private WebClient webClient;
 
     public String healthCheck(){
         String status;
-        Mono<String> test = webClient.get()
-                .uri(healthCheckURL)
-                .header("x-org-id", "health.fintlabs.no " + Base64Utils
-                        .encodeToString(("").getBytes(UTF_8)))
-                .retrieve().bodyToMono(String.class);
+        webClient = WebClient.builder()
+                .baseUrl("https://play-with-fint.felleskomponent.no/utdanning/timeplan/admin/health")
+                .defaultHeader("x-client", "testbruker")
+                .defaultHeader("x-org-id", "health.fintlabs.no").build();
+        Mono<String> test = webClient.get().exchange().block().bodyToMono(String.class);
         status = test.block();
         return status;
     }
