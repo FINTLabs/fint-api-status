cd4359c0ddb37b3e77ef061b9daeb52a0fcab3a6
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-02-20 07:07:27 +0100

    added webclient config

--- src/main/java/no/fint/fintapistatus/controller/Controller.java
+++ src/main/java/no/fint/fintapistatus/controller/Controller.java
@@ -1,5 +1,6 @@
 package no.fint.fintapistatus.controller;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,30 +8,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-@RequestMapping(value = "/api/")
+@RequestMapping(value = "/api")
 public class Controller {
-@Autowired
-private PeopleService peopleService;
-@Autowired
-HealthService healthService;
+    @Autowired
+    private PeopleService peopleService;
+    @Autowired
+    private HealthService healthService;
 
     @GetMapping(value = "/people")
-    public List<People> getPeople(){
+    public List<People> getPeople() {
         return peopleService.getAllPeople();
     }
 
     @GetMapping(value = "/test/{var}")
-    public String getVar(@PathVariable final String var){
-
-
+    public String getVar(@PathVariable final String var) {
         return var;
     }
+
     @GetMapping(value = "/healthcheck")
-    public String getHealthCheckStatus(){
+    public String getHealthCheckStatus() {
         String status = healthService.healthCheck();
         return status;
     }
