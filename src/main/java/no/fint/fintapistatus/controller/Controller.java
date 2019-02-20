44a0dbe265018c4881000693968baf19a0bd506a
Ola <olastaalberg@gmail.com>
2019-02-20 22:26:10 +0100

    Denne lager nå en healthcheck som går gjennom alle domenene. Tar laaang tid, men fungerer. Må sjekke opp hvorfor det tar så lang tid og hvorfor det er så mange som feiler hele tiden? Er serverne nede?

--- src/main/java/no/fint/fintapistatus/controller/Controller.java
+++ src/main/java/no/fint/fintapistatus/controller/Controller.java
@@ -1,6 +1,8 @@
 package no.fint.fintapistatus.controller;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,12 @@ public class Controller {
     @Autowired
     private HealthService healthService;
 
+    private TreeMap<String, LinkedList<String>> domenekart = new TreeMap<>();
+    private String domene1 = "administrasjon";
+    private String domene2 = "utdanning";
+    private LinkedList<String> domene1Underdomener = new LinkedList<>();
+    private LinkedList<String> domene2Underdomener = new LinkedList<>();
+
     @GetMapping(value = "/people")
     public List<People> getPeople() {
         return peopleService.getAllPeople();
@@ -28,7 +36,24 @@ public class Controller {
 
     @GetMapping(value = "/healthcheck")
     public String getHealthCheckStatus() {
-        String status = healthService.healthCheck();
+        domene1Underdomener.add("personal");
+        domene1Underdomener.add("organisasjon");
+        domene1Underdomener.add("kodeverk");
+        domenekart.put(domene1, domene1Underdomener);
+        domene2Underdomener.add("elev");
+        domene2Underdomener.add("utdanningsprogram");
+        domene2Underdomener.add("vurdering");
+        domene2Underdomener.add("timeplan");
+        domene2Underdomener.add("kodeverk");
+        domenekart.put(domene2, domene2Underdomener);
+        return makeReadable(healthService.healthCheck(domenekart));
+    }
+    private String makeReadable(String status) {
+        if (status != null && status.length() > 50) {
+            status = status.replace("{", "{<br>");
+            status = status.replace("}", "}<br>");
+            System.out.println(status);
+        }
         return status;
     }
 }
--- src/main/java/no/fint/fintapistatus/controller/HealthService.java
+++ src/main/java/no/fint/fintapistatus/controller/HealthService.java
@@ -2,11 +2,11 @@ package no.fint.fintapistatus.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class HealthService {
@@ -15,14 +15,26 @@ public class HealthService {
     @Autowired
     private WebClient webClient;
 
-    public String healthCheck(){
-        String status;
-        webClient = WebClient.builder()
-                .baseUrl("https://play-with-fint.felleskomponent.no/utdanning/timeplan/admin/health")
-                .defaultHeader("x-client", "testbruker")
-                .defaultHeader("x-org-id", "health.fintlabs.no").build();
-        Mono<String> test = webClient.get().exchange().block().bodyToMono(String.class);
-        status = test.block();
-        return status;
+    public String healthCheck(Map<String, LinkedList<String>> domenekart){
+        StringBuilder status = new StringBuilder();
+
+        Set<String> domene1nøkkel = domenekart.keySet();
+
+        for (String hoveddomene : domene1nøkkel) {
+            LinkedList<String> underdomener = domenekart.get(hoveddomene);
+            for (String underdomene : underdomener){
+                String nyHealthCheckURL = String
+                        .format("https://play-with-fint.felleskomponent.no/%s/%s/admin/health", hoveddomene,underdomene);
+                status.append("<br><br>Nå tester vi:<br>"+nyHealthCheckURL+ "<br><br>");
+                webClient = WebClient.builder()
+                        .baseUrl(nyHealthCheckURL)
+                        .defaultHeader("x-client", "testbruker")
+                        .defaultHeader("x-org-id", "fint.health")
+                        .build();
+                Mono<String> test = webClient.get().exchange().block().bodyToMono(String.class);
+                status.append(test.block());
+            }
+        }
+        return status.toString();
     }
 }
