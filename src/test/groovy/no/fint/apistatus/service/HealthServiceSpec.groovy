1094601812b78b7da21d47e02e4cd0c7ce97770b
Jarle Hansen <jarle@jarlehansen.net>
2019-04-06 20:48:22 +0200

    started fixing HealthServiceSpec

--- src/test/groovy/no/fint/apistatus/service/HealthServiceSpec.groovy
+++ src/test/groovy/no/fint/apistatus/service/HealthServiceSpec.groovy
@@ -12,59 +12,81 @@ import spock.lang.Specification
 
 class HealthServiceSpec extends Specification {
 
-    private def mockWebServer = new MockWebServer()
-    private def componentServiceSuccess = Mock(ComponentService) {
-        getComponents() >> [new ComponentConfiguration(
-                name: "administrasjon-personal",
-                port: 0,
-                path: "/administrasjon/personal",
-                assetPath: "/api/components/assets/administrasjon/personal")]
-}
-    private def componentServiceFailing = Mock(ComponentService) {
-        getComponents() >> [new ComponentConfiguration(
-                name: "a-failing-uri",
-                port: 0,
-                path: "/failing/uri",
-                assetPath: "/api/components/assets/failing/uri")]
+    private def server = new MockWebServer()
+
+    private def successComponent = new ComponentConfiguration(
+            name: "administrasjon-personal",
+            port: 0,
+            path: "/administrasjon/personal",
+            assetPath: "/api/components/assets/administrasjon/personal"
+    )
+    private def failedComponent = new ComponentConfiguration(
+            name: "a-failing-uri",
+            port: 0,
+            path: "/failing/uri",
+            assetPath: "/api/components/assets/failing/uri"
+    )
+
+    private def componentService = Mock(ComponentService)
+
+    def healthService = new HealthService(componentService: componentService,
+            webClient: WebClient.create(server.url('/').toString()))
+
+    def successResponse = new MockResponse().setBody(new ClassPathResource("healthcheckSuccess.json").getFile().text)
+    def failResponse = new MockResponse().setBody(new ClassPathResource("healthcheckFailed.json").getFile().text)
+
+    void cleanup() {
+        server.shutdown()
     }
-    private def healthService = new HealthService(baseUrl: mockWebServer.url('/').toString(),
-            webClient: WebClient.create())
 
-
-    def "Check single component and return Mono<Event>-object"() {
+    def "Health check one component return last event"() {
         given:
-        def healthService = new HealthService(baseUrl: "https://play-with-fint.felleskomponent.no/", webClient: WebClient.create())
+        server.enqueue(successResponse)
 
         when:
-        def healthCheck = healthService.healthCheck("administrasjon/personal")
+        healthService.healthCheckOne('administrasjon/personal')
+        def healthChecks = healthService.getHealthChecks()
 
         then:
-        healthCheck
+        healthChecks.size() == 1
+        healthChecks[0].apiBaseUrl == 'administrasjon/personal'
+        healthChecks[0].event
+    }
+
+    def "Multiple health checks on same component returns last check only"() {
+        given:
+        server.enqueue(successResponse)
+        server.enqueue(successResponse)
+
+        when:
+        healthService.healthCheckOne('administrasjon/personal')
+        healthService.healthCheckOne('administrasjon/personal')
+        def healthChecks = healthService.getHealthChecks()
+
+        then:
+        healthChecks.size() == 1
     }
 
     def "Run healthcheck on all components"() {
         given:
-        def healthService = new HealthService(baseUrl: mockWebServer.url('/').toString(),
-                componentService: componentServiceSuccess)
-        healthService.init()
-        def jsonResponse = new ClassPathResource("healthcheckSuccess.json").getFile().text
-        mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).setBody(jsonResponse))
+        server.enqueue(successResponse)
 
         when:
         healthService.healthCheckAll()
+        def healthChecks = healthService.getHealthChecks()
 
         then:
-        healthService.status.size() == 1
-        healthService.status.values()[0].data[0]["status"] == HealthStatus.APPLICATION_HEALTHY.name()
-        healthService.status.values()[0].data[1]["status"] == HealthStatus.RECEIVED_IN_CONSUMER_FROM_PROVIDER.name()
+        1 * componentService.getComponents() >> [successComponent]
+        healthChecks.size() == 1
     }
+
     def "Check that an event is created when healthcheck fails and that it got timestamp"() {
         given:
-        def healthService = new HealthService(baseUrl: mockWebServer.url('/').toString(),
+        def healthService = new HealthService(baseUrl: server.url('/').toString(),
                 componentService: componentServiceFailing)
         healthService.init()
         def jsonResponse = new ClassPathResource("healthcheckFailed.json").getFile().text
-        mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).setBody(jsonResponse))
+        server.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).setBody(jsonResponse))
 
         when:
         healthService.healthCheckAll()
@@ -75,13 +97,14 @@ class HealthServiceSpec extends Specification {
         healthService.status.values()[0].data[0] != HealthStatus.APPLICATION_HEALTHY.name()
         healthService.status.values()[0].data[1] != HealthStatus.RECEIVED_IN_CONSUMER_FROM_PROVIDER.name()
     }
+
     def "Run healthcheck on a single path"() {
         given:
-        def healthService = new HealthService(baseUrl: mockWebServer.url('/').toString(),
+        def healthService = new HealthService(baseUrl: server.url('/').toString(),
                 componentService: componentServiceSuccess)
         healthService.init()
         def jsonResponse = new ClassPathResource("healthcheckSuccess.json").getFile().text
-        mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).setBody(jsonResponse))
+        server.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).setBody(jsonResponse))
 
         when:
         healthService.healthCheckOne("/administrasjon/personal")
