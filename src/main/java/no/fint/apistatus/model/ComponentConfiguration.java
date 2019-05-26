d457f7522c6259ccf93b2496384952baf7f574c5
Frode <frode@sjovatsen.net>
2019-05-26 14:13:29 +0200

    added support for pwf

--- src/main/java/no/fint/apistatus/model/ComponentConfiguration.java
+++ src/main/java/no/fint/apistatus/model/ComponentConfiguration.java
@@ -5,9 +5,9 @@ import lombok.Data;
 @Data
 public class ComponentConfiguration {
     private String name;
-    private Integer port;
     private String path;
     private String assetPath;
     private boolean isInProduction;
     private boolean isInBeta;
+    private boolean isInPlayWithFint;
 }
